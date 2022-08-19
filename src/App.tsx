e8b3c44c2d7172f853884c796941bae586bd186f
malcolmlindberg <malcolm.lindberg@hotmail.se>
2022-08-19 11:07:59 +0200

    Contact updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Contact } from "./pages/contact";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
