6d024152054d9507c61b49072b9f9bbb69c8b0c4
malcolmlindberg <malcolm.lindberg@hotmail.se>
2022-08-19 11:13:12 +0200

    Routing setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Contact } from "./pages/contact";
+import { Home } from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
