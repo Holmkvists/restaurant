62333be3d6191b7d302ea2a9d19bc8f684148775
Metin Ahmad <metinmayi@gmail.com>
2022-08-23 18:08:10 +0200

    Added AdminTableHead Component

---
+++ src/pages/admin/AdminTable/AdminTableHead/AdminTableHead.tsx
@@ -0,0 +1,13 @@
+export const AdminTableHead: React.FC = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Tables</th>
+        <th>Name</th>
+        <th>Booking Ref.</th>
+      </tr>
+    </thead>
+  );
+};
