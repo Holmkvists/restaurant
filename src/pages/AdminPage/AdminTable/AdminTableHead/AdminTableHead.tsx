4ef228d787ea1168de89deb7f219f2d9ddef3619
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-27 14:25:30 +0200

    Created AdminTableHead

---
+++ src/pages/AdminPage/AdminTable/AdminTableHead/AdminTableHead.tsx
@@ -0,0 +1,11 @@
+export const AdminTableHead = () => {
+  return (
+    <thead>
+      <th>Email</th>
+      <th>Date</th>
+      <th>Time</th>
+      <th>Tables</th>
+      <th>ID</th>
+    </thead>
+  );
+};
