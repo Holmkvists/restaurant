8c84ba435ff2a37ee7b08067d46555b0c627ad1d
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-28 21:13:59 +0200

    AdminTableBody now gets all bookings

--- src/pages/AdminPage/AdminTable/AdminTableBody/AdminTableBody.tsx
+++ src/pages/AdminPage/AdminTable/AdminTableBody/AdminTableBody.tsx
@@ -1,9 +1,25 @@
+import { getAllBookings } from "api/api";
+import { IBooking } from "models/IBooking";
+import { useEffect, useState } from "react";
 import { AdminTableRow } from "./AdminTableRow/AdminTableRow";
 
 export const AdminTableBody = () => {
+  const [bookings, setBookings] = useState<IBooking[]>([]);
+
+  useEffect(() => {
+    async function getBookings() {
+      const bookings = await getAllBookings();
+      setBookings(bookings);
+      console.log(bookings);
+    }
+    getBookings();
+  }, []);
+
   return (
     <tbody>
-      <AdminTableRow />
+      {bookings.map((booking) => (
+        <AdminTableRow booking={booking} key={booking._id} />
+      ))}
     </tbody>
   );
 };
