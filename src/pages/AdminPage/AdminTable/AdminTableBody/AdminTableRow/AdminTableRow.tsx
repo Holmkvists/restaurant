e8600493cb1729661853dc0f58601f45e278a6a0
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-28 21:58:42 +0200

    Added TableRow

--- src/pages/AdminPage/AdminTable/AdminTableBody/AdminTableRow/AdminTableRow.tsx
+++ src/pages/AdminPage/AdminTable/AdminTableBody/AdminTableRow/AdminTableRow.tsx
@@ -1,23 +1,80 @@
-import { GenericButton } from "../../../../../components/GenericButton/GenericButton";
+import { patchBooking } from "api/api";
+import { GenericButton } from "components/GenericButton/GenericButton";
+import { IBooking } from "models/IBooking";
+import { useState } from "react";
+import "./styles/adminTableRow.css";
 
-export const AdminTableRow = () => {
+export const AdminTableRow = (props: AdminTableRowProps) => {
+  const id = props.booking._id;
+  const email = props.booking.email;
+
+  const [date, setDate] = useState(props.booking.date);
+  const [time, setTime] = useState(props.booking.time);
+  const [tables, setTables] = useState(props.booking.tables);
+
+  const [isEditable, setIsEditable] = useState(false);
+  const [style, setStyle] = useState({ backgroundColor: "transparent" });
+
+  const handleEdit = () => {
+    setIsEditable(!isEditable);
+    const newStyle =
+      style.backgroundColor === "transparent" ? "#ffbf00" : "transparent";
+    setStyle({ backgroundColor: newStyle });
+  };
+
+  const handleSave = async () => {
+    const booking: IBooking = {
+      _id: id,
+      email,
+      date,
+      time,
+      tables,
+    };
+    await patchBooking(booking);
+  };
+
   return (
     <tr>
-      <td>metinmayi@gmail.com</td>
-      <td>2022-01-15</td>
-      <td>18:00</td>
-      <td>5</td>
-      <td>asdsa98231839sad</td>
+      <td>{email}</td>
+      <td>{id}</td>
+      <td
+        style={style}
+        suppressContentEditableWarning
+        contentEditable={isEditable}
+        onBlur={(e) => setDate(e.currentTarget.innerText)}
+      >
+        {date}
+      </td>
+      <td
+        style={style}
+        suppressContentEditableWarning
+        contentEditable={isEditable}
+        onBlur={(e) => setTime(e.currentTarget.innerText)}
+      >
+        {time}
+      </td>
+      <td
+        style={style}
+        suppressContentEditableWarning
+        contentEditable={isEditable}
+        onBlur={(e) => setTables(e.currentTarget.innerText)}
+      >
+        {tables}
+      </td>
       <td>
-        <GenericButton size="s" handleClick={() => console.log("hey")}>
+        <GenericButton size="s" handleClick={(e) => handleEdit()}>
           Edit
         </GenericButton>
       </td>
       <td>
-        <GenericButton size="s" handleClick={() => console.log("hey")}>
+        <GenericButton size="s" handleClick={() => handleSave()}>
           Save
         </GenericButton>
       </td>
     </tr>
   );
 };
+
+interface AdminTableRowProps {
+  booking: IBooking;
+}
