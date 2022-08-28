a22e460f44e26e90ccc4060ae075ff0dcef8b4ea
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-28 16:34:46 +0200

    Changed order of THs in table head

--- src/pages/AdminPage/AdminTable/AdminTableHead/AdminTableHead.tsx
+++ src/pages/AdminPage/AdminTable/AdminTableHead/AdminTableHead.tsx
@@ -1,12 +1,15 @@
+import "./styles/adminTableHead.css";
+
 export const AdminTableHead = () => {
   return (
     <thead>
       <tr>
         <th>Email</th>
+        <th>ID</th>
         <th>Date</th>
         <th>Time</th>
         <th>Tables</th>
-        <th>ID</th>
+        <th colSpan={2}>Manage</th>
       </tr>
     </thead>
   );
