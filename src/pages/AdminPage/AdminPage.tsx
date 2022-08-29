dfeb04351d8e877b07f02d26f3786f70ec4a369d
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-29 22:29:08 +0200

    Added admin auth to the adminPage

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import { AdminLogin } from "./AdminLogin/AdminLogin";
 import { AdminTable } from "./AdminTable/AdminTable";
 import "./styles/adminPage.css";
 
 export const AdminPage = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
-    <>
-      <AdminTable />
-    </>
+    <div className="admin__wrapper">
+      {isAdmin ? <AdminTable /> : <AdminLogin setIsAdmin={setIsAdmin} />}
+    </div>
   );
 };
