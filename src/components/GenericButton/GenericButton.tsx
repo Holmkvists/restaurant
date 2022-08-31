c1b9e350d26caa177bdeca7a09827d544faac176
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-31 20:58:22 +0200

    Removed fontSize prop from GenericButton

--- src/components/GenericButton/GenericButton.tsx
+++ src/components/GenericButton/GenericButton.tsx
@@ -3,14 +3,14 @@ import "./styles/genericButton.css";
 interface IGenericProps {
   children: string;
   size: "s" | "m" | "l";
-  fontSize: "s" | "m" | "l";
   handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const GenericButton = (props: IGenericProps) => {
   const styleObject = {
-    width: props.size == "s" ? "50px" : props.size == "m" ? "100px" : "150px",
-    fontSize: props.size == "s" ? "10px" : props.size == "m" ? "20px" : "30px",
+    width: props.size === "s" ? "50px" : props.size === "m" ? "100px" : "150px",
+    fontSize:
+      props.size === "s" ? "10px" : props.size === "m" ? "20px" : "30px",
   };
 
   return (
